1caafb45542e75b84d593aeb8511bc32ead3fd9b
limelime <limelime@users.noreply.github.com>
2017-01-20 11:39:27 -0500

    ADD:

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+# Description: Deploy scripts to different locations.
+
+yes | cp -av ./xwinsplitter*.sh /usr/local/bin/
+
+yes | cp -av xwinsplitter*.sh /media/master/github/cust-live-deb/scripts/repository/inst-min-win-cld-win-split/xwinsplitter
